ca942d47a81120b055032b5ec262019e5eaf0eb1
Naili rohmah <nailirohmah292@gmail.com>
2025-08-25 17:24:07 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('index'); // halaman Home
+});
+
+Route::get('/about', function () {
+    return view('partials.about');
+});
+
+Route::get('/shop', function () {
+    return view('partials.shop');
+});
+
+Route::get('/contact', function () {
+    return view('partials.contact');
 });
