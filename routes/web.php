5de914b150cc63464da3578d6612adce82e591e6
Naili rohmah <nailirohmah292@gmail.com>
2025-08-26 16:49:17 +0700

    layout

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index'); // halaman Home
+Route::get('index', function () {
+    return view('index'); 
 });
 
 Route::get('/about', function () {
     return view('partials.about');
-});
-
-Route::get('/shop', function () {
-    return view('partials.shop');
 });
 
 Route::get('/contact', function () {
